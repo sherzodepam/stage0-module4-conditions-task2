d8f5e79635a4a5be108b1305763b39a43e908dae
Sherzod <sherzodd7707@gmail.com>
2022-09-27 10:44:53 +0500

    first

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-        if(from1UpTo1000>500){
+        if(from1UpTo1000<500){
             System.out.println("Eagle");
         }
         else{
